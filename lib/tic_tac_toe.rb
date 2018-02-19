01630e925e8df2f0f443872a04c4e309ed8ba387
Yevgeniy Zhitomirskiy <yevgeniy.zhitomirskiy@wework.com>
2018-02-19 16:21:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,24 +91,7 @@ def winner(board)
 end
 
 
-##################
-# Helper Methods #
-##################
-def valid_move?(board,index)
-  return index.between?(0,8) && !position_taken?(board,index)
-end
 
-def input_to_index(input)
-  return input.to_i - 1
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
 def position_taken?(board, index)
   if (board[index] == " ") then
